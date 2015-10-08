0424550b7417d6db99511c6d56a58f1d0d05a5db
Jimmy Kim <jkimab@ust.hk>
2015-10-09 02:22:57 +0800

    Created 3 fragments with a MainActivity that extends tablistener
    Created 3 fragments with a MainActivity that extends tablistener

---
+++ Pickup/app/src/main/java/com/example/kim/pickup/fragment/DistanceFragment.java
@@ -0,0 +1,46 @@
+package com.example.kim.pickup.fragment;
+
+/**
+ * Created by kim on 2015-10-08.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kim.pickup.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DistanceFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static DistanceFragment newInstance(int sectionNumber) {
+        DistanceFragment fragment = new DistanceFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public DistanceFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_distance, container, false);
+        return rootView;
+    }
+}
---
+++ Pickup/app/src/main/java/com/example/kim/pickup/fragment/PopularFragment.java
@@ -0,0 +1,46 @@
+package com.example.kim.pickup.fragment;
+
+/**
+ * Created by kim on 2015-10-08.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kim.pickup.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PopularFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PopularFragment newInstance(int sectionNumber) {
+        PopularFragment fragment = new PopularFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PopularFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_popular, container, false);
+        return rootView;
+    }
+}
