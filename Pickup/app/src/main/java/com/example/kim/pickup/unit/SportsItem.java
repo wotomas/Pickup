5e509bfcfe7408246dd96072027da44ffd1b79b8
Jimmy Kim <jkimab@ust.hk>
2015-10-09 02:24:02 +0800

    Created a new Unit. Sports Item Unit
    Contains a textview and a thumbnail image

---
+++ Pickup/app/src/main/java/com/example/kim/pickup/unit/SportsItem.java
@@ -0,0 +1,27 @@
+package com.example.kim.pickup.unit;
+
+/**
+ * Created by kim on 2015-10-08.
+ */
+public class SportsItem {
+    private String _name;
+    private int _thumbnail;
+
+    public int get_thumbnail() {
+        return _thumbnail;
+    }
+
+    public void set_thumbnail(int _thumbnail) {
+        this._thumbnail = _thumbnail;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+
+}
